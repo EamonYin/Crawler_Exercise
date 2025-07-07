d534a1684a05f1b8011c8fba9fbba1da01dae93a
EamonYin <xiaominglovejava@163.com>
2025-07-07 14:23:23 +0800

    初次提交

---
+++ src/main/java/com/crawler/crawler_exercise/CrawlerExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.crawler.crawler_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlerExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrawlerExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crawler/crawler_exercise/CrawlerExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crawler.crawler_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlerExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

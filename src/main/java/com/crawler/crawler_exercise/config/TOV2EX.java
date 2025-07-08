8ef43aa5596b708ef76a2956cb0e9439911f6e84
EamonYin <xiaominglovejava@163.com>
2025-07-08 16:26:52 +0800

    点击到了新页面

--- src/main/java/com/crawler/crawler_exercise/config/TOV2EX.java
+++ src/main/java/com/crawler/crawler_exercise/config/TOV2EX.java
@@ -3,11 +3,13 @@ package com.crawler.crawler_exercise.config;
 import com.crawler.crawler_exercise.entiy.V2EXInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,10 +32,18 @@ public class TOV2EX {
         for (int i = 1; i < elements.size(); i++) {
             V2EXInfo v2EXInfo = new V2EXInfo();
             // 获取内层元素
-            WebElement titleElement = elements.get(i).findElement(By.cssSelector(".item_title"));
+            WebElement v2EXElement = elements.get(i);
+            WebElement titleElement = v2EXElement.findElement(By.cssSelector(".item_title"));
+            // 点击
+            titleElement.click();
+            log.info("[点击]新页面地址:{}",chromeDriver.getCurrentUrl());
+            log.info("[点击]新页面的标题:{}",chromeDriver.getTitle());
+
+            // 获取内容
             String title = titleElement.getText();
             v2EXInfo.setSort(i);
             v2EXInfo.setTitle(title);
+            v2EXInfo.setInfo(chromeDriver.getTitle());
             titleLst.add(v2EXInfo);
 //            System.out.println("getV2EXInfo输出内容:【" + title+"】");
         }

9a9a56c467e3c63ea106bfa8ff514372a546097d
EamonYin <xiaominglovejava@163.com>
2025-08-01 14:17:03 +0800

    引入redis

--- src/test/java/com/crawler/crawler_exercise/CrawlerExerciseApplicationTests.java
+++ src/test/java/com/crawler/crawler_exercise/CrawlerExerciseApplicationTests.java
@@ -4,7 +4,9 @@ import com.crawler.crawler_exercise.config.YunWuConfig;
 import com.crawler.crawler_exercise.entiy.CrawlerInfo;
 import com.crawler.crawler_exercise.service.ICrawlerInfoService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.annotation.Resource;
 
@@ -13,6 +15,8 @@ class CrawlerExerciseApplicationTests {
 
     @Resource
     ICrawlerInfoService crawlerInfoService;
+    @Autowired
+    private StringRedisTemplate redisTemplate;
 
     @Test
     void mysqlTest() {
@@ -20,6 +24,17 @@ class CrawlerExerciseApplicationTests {
         crawlerInfo.setInfo("第一条数据");
         crawlerInfoService.insertCrawlerInfo(crawlerInfo);
 
+    }
+
+    @Test
+    void testRedis() {
+        // 写入数据
+        redisTemplate.opsForValue().set("test_key", "Hello Spring Boot Redis!");
+
+        // 读取数据
+        String value = redisTemplate.opsForValue().get("test_key");
+
+        System.out.println("Redis value: " + value);
     }
 
 }

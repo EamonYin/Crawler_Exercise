fab7944ec9ad280bfe21345fef38ca1dda0ad700
EamonYin <xiaominglovejava@163.com>
2025-08-26 11:16:11 +0800

    fix: update sh

--- crawler.sh
+++ crawler.sh
@@ -1,6 +1,10 @@
-cd /Users/eamonyin/做过的项目/Crawler_Exercise
-docker build -t crawler:v1 .
-docker stop crawler
-docker rm crawler
-docker run -itd -p 0.0.0.0:8085:8083 --name Crawler_Exercise crawler:v1
+# 第一步：清理target目录
+mvn clean
+# 第二步：打包并指定JAR名称
+mvn package
+# 第二步：Docker构建
+docker build -t crawler:v5 .
+docker stop Crawler_Exercise
+docker image rm crawler:v4
+docker run -itd -p 0.0.0.0:8085:8083 --name Crawler_Exercise crawler:v5
 docker logs -f Crawler_Exercise

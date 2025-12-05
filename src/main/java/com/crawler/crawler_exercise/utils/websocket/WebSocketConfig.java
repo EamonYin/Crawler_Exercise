33b9f2063e89f326005e0644581af761d6d83e84
EamonYin <xiaominglovejava@163.com>
2025-12-05 16:27:00 +0800

    fix:处理单元测试websocket报错的问题

--- src/main/java/com/crawler/crawler_exercise/utils/websocket/WebSocketConfig.java
+++ src/main/java/com/crawler/crawler_exercise/utils/websocket/WebSocketConfig.java
@@ -1,10 +1,12 @@
 package com.crawler.crawler_exercise.utils.websocket;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
+@ConditionalOnProperty(name = "websocket.enabled", havingValue = "true", matchIfMissing = false)
 public class WebSocketConfig {
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
